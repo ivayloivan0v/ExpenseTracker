67562a25045bb6088a4f6b049dbb61a3a937e34d
Ivaylo <ivayloivan0v@icloud.com>
2022-09-20 23:10:15 +0300

    adding a chart

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import { ChartBar } from "./ChartBar";
+import "./Chart.css";
+
+export const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+          key={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
