fbfad6380032532d655814fd2eb9b79c68ec464d
Ivaylo <ivayloivan0v@icloud.com>
2022-09-20 04:43:07 +0300

    working with events and state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,30 +1,41 @@
 import "./Expenses.css";
 import { ExpenseItem } from "./ExpenseItem";
 import { Card } from "../UI/Card";
+import { ExpensesFilter } from "./ExpensesFilter";
+import { useState } from "react";
 
 export const Expenses = ({ item }) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={item[0].title}
-        amount={item[0].amount}
-        date={item[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={item[1].title}
-        amount={item[1].amount}
-        date={item[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={item[2].title}
-        amount={item[2].amount}
-        date={item[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={item[3].title}
-        amount={item[3].amount}
-        date={item[3].date}
-      ></ExpenseItem>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpenseItem
+          title={item[0].title}
+          amount={item[0].amount}
+          date={item[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={item[1].title}
+          amount={item[1].amount}
+          date={item[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={item[2].title}
+          amount={item[2].amount}
+          date={item[2].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={item[3].title}
+          amount={item[3].amount}
+          date={item[3].date}
+        ></ExpenseItem>
+      </Card>
+    </div>
   );
 };
